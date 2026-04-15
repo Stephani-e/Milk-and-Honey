39bfe20150e670c4b429c9975dec3b74d14f7022
Kipperliveth <godsonamadi687@gmail.com>
2026-04-15 23:18:19 +0100

    publishable keys update

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUB_KEY!
 
 // Use createBrowserClient instead of createClient
 export const supabase = createBrowserClient(supabaseUrl, supabaseAnonKey)
